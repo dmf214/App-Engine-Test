e80d5dd40b411bb1a3787a73ca981cb838395eb1
dmf214 <revolution0007@gmail.com>
2014-10-01 13:53:18 -0400

    test

--- Hello World/src/myTest/HelloWorld.java
+++ Hello World/src/myTest/HelloWorld.java
@@ -2,7 +2,8 @@ package myTest;
 
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("Hello World"){
+		System.out.println("Hello World");
+		//Added a comment
 		}	
 	}
 }	
